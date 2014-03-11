1888f075772be008dc8f382143db969429e51fc5
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2014-03-11 17:45:45 +0200

    A few changes, but still the roxygen2 is failing.

--- rubbish/testing_OmniDriver.r
+++ rubbish/testing_OmniDriver.r
@@ -1,12 +1,15 @@
+options(java.home="C:\\Program Files\\Ocean Optics\\OmniDriver\\_jvm")
 library(rJava)
 
-# Sys.setenv(PATH=".;C:\\Program Files\\Ocean Optics\\OmniDriver\\_jvm")
+f <- .jnew("java/awt/Frame","Hello")
+.jcall(f,,"setVisible",TRUE)
+
+Sys.setenv(PATH=".;C:\\Program Files\\Ocean Optics\\OmniDriver\\_jvm")
 # OO_path <- paste(Sys.getenv("OOI_HOME"),"\\OmniDriver.jar", sep="")
 OO_path <- Sys.getenv("OOI_HOME")
-# Sys.setenv(CLASSPATH=paste(".;",OO_path,sep=""))
 .jinit(force.init=TRUE)
 .jaddClassPath(OO_path)
-.jaddClassPath(dir( "./inst/java", full.names=TRUE ))
+# .jaddClassPath(dir( "./inst/java", full.names=TRUE ))
 
 .jclassPath()
 
