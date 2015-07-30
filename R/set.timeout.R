b5fc5bbd8b67a9235c05c72281e12a024c4c326d
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2015-07-30 16:28:14 +0300

    Minor documentation edit. Rebuild under R 3.2.1

--- R/set.timeout.R
+++ R/set.timeout.R
@@ -1,6 +1,7 @@
 #' Set "timeout" for addressed spectrometer
 #' useful when used together with triggers
 #' 
+#' @param jwrapper an open Wrapper object from Omnidriver
 #' @param time.millisec time to wait in milliseconds (an integer)
 #' @param sr.index an index to address the spectrometer 
 #' @keywords manip misc
