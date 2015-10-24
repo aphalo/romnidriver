7c4bbb9081120a08ea6d5b803f726d605b6ae1b1
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2015-10-24 12:57:46 +0300

    Add channel-related methods from WrapperExtensions

--- R/get.detector.R
+++ R/get.detector.R
@@ -2,18 +2,16 @@
 #' 
 #' @param jwrapper an open Wrapper object from Omnidriver
 #' @param sr.index an index to address the spectrometer
-#' @param ch.index an index to address a channel
 #' @param ch.index an index to address the channel in a spectrometer with more
 #'   than one channel.
 #' @keywords manip misc
 #' @export
-#' @return a numeric value
-#' @note This method is only of use for multichannel spectrometers.
+#' @return a Detector object (Java)
+#' @note This method seems to be only of use for multichannel spectrometers. It 
+#'   is not documented in the OmniDriver manual and the Java documentation is
+#'   extremely terse so it is difficult to know what it does and/or how to use 
+#'   it.
 #'
-get_detector <- function(jwrapper, sr.index=0L, ch.index=NULL){
-  if (is.null(ch.index)) {
-    jwrapper$getDetector(as.integer(sr.index), 0L)
-  } else {
-    jwrapper$getDetector(as.integer(sr.index), as.integer(ch.index))
-  }
+get_detector <- function(jwrapper, sr.index=0L, ch.index=0L){
+  jwrapper$getDetector(as.integer(sr.index), as.integer(ch.index))
 }
