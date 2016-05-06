e10046d1c14f12b0c754368de5fa30a46c11dbbb
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2016-05-06 17:56:39 +0300

    Update dependency versions.

--- vignettes/user-guide.R
+++ vignettes/user-guide.R
@@ -1,4 +1,10 @@
 ## ------------------------------------------------------------------------
+library(knitr)
+# To build the User Guide with an instrument connected change to TRUE
+eval_switch <- TRUE
+opts_chunk$set(eval=eval_switch)
+
+## ------------------------------------------------------------------------
 library(rOmniDriver)
 w <- init_api()
 get_api_version(w)
@@ -7,25 +13,29 @@ get_api_version(w)
 n <- open_all_spectrometers(w)
 n
 
-## ---- eval=n>0L----------------------------------------------------------
+## ------------------------------------------------------------------------
+eval_switch <- eval_switch && n
+opts_chunk$set(eval=eval_switch)
+
+## ------------------------------------------------------------------------
 #  get_name(jwrapper = w, sr.index = 0L)
 #  get_serial_number(jwrapper = w, sr.index = 0L)
 
-## ---- eval=n>0L----------------------------------------------------------
+## ------------------------------------------------------------------------
 #  get_name(w)
 #  get_serial_number(w)
 
-## ---- eval=n>0L----------------------------------------------------------
+## ------------------------------------------------------------------------
 #  bench <- get_bench(jwrapper = w, sr.index = 0L)
 #  bench$getGrating()
 #  bench$getFilterWavelength()
 #  bench$getSlitSize()
 
-## ---- eval=n>0L----------------------------------------------------------
+## ------------------------------------------------------------------------
 #  n_ch <- get_number_of_channels(jwrapper = w, sr.index = 0L)
 #  n_ch
 
-## ---- eval=n>0L----------------------------------------------------------
+## ------------------------------------------------------------------------
 #  get_maximum_integration_time(jwrapper = w, sr.index = 0L)
 #  get_minimum_integration_time(jwrapper = w, sr.index = 0L)
 #  get_maximum_intensity(jwrapper = w, sr.index = 0L)
@@ -36,7 +46,7 @@ n
 #  get_number_of_pixels(jwrapper = w, sr.index = 0L, ch.index = 0L)
 #  get_number_of_dark_pixels(jwrapper = w, sr.index = 0L, ch.index = 0L)
 
-## ---- eval=n>0L----------------------------------------------------------
+## ------------------------------------------------------------------------
 #  if (is_feature_supported_board_temperature(jwrapper = w, sr.index = 0L)) {
 #    controller.board <-
 #      get_feature_controller_board_temperature(jwrapper = w, sr.index = 0L)
@@ -45,7 +55,7 @@ n
 #    print("feature not supported")
 #  }
 
-## ---- eval=n>0L----------------------------------------------------------
+## ------------------------------------------------------------------------
 #  if (is_feature_supported_detector_temperature(jwrapper = w, sr.index = 0L)) {
 #    detector.board <-
 #      get_feature_detector_board_temperature(jwrapper = w, sr.index = 0L)
@@ -54,7 +64,7 @@ n
 #    print("feature not supported")
 #  }
 
-## ---- eval=n>0L----------------------------------------------------------
+## ------------------------------------------------------------------------
 #  if (is_feature_supported_spectrum_type(jwrapper = w, sr.index = 0L)) {
 #    spectrum.type <- get_feature_spectrum_type(jwrapper = w, sr.index = 0L)
 #    spectrum.type$setSpectrumType(1L) # raw
@@ -63,36 +73,36 @@ n
 #    print("feature not supported")
 #  }
 
-## ---- eval=n>0L----------------------------------------------------------
+## ------------------------------------------------------------------------
 #  set_integration_time(jwrapper = w, time.usec = 5e6, sr.index = 0L, ch.index = 0L)
 #  get_integration_time(jwrapper = w, sr.index = 0L, ch.index = 0L)
 
-## ---- eval=n>0L----------------------------------------------------------
+## ------------------------------------------------------------------------
 #  set_boxcar_width(w, half.width.px = 0L, sr.index = 0L, ch.index = 0L)
 #  get_boxcar_width(jwrapper = w, sr.index = 0L, ch.index = 0L)
 
-## ---- eval=n>0L----------------------------------------------------------
+## ------------------------------------------------------------------------
 #  set_scans_to_avg(jwrapper = w, n.scans = 4L, sr.index = 0L, ch.index = 0L)
 #  get_scans_to_avg(jwrapper = w, sr.index = 0L, ch.index = 0L)
 
-## ---- eval=n>0L----------------------------------------------------------
+## ------------------------------------------------------------------------
 #  set_correct_for_detector_nonlinearity(jwrapper = w, correct = 0L, sr.index = 0L, ch.index = 0L)
 #  get_correct_for_detector_nonlineary(jwrapper = w, sr.index = 0L, ch.index = 0L)
 
-## ---- eval=n>0L----------------------------------------------------------
+## ------------------------------------------------------------------------
 #  set_correct_for_electrical_dark(jwrapper = w, enable = 0L, sr.index = 0L)
 #  get_correct_for_electrical_dark(jwrapper = w, sr.index = 0L)
 
-## ---- eval=n>0L----------------------------------------------------------
+## ------------------------------------------------------------------------
 #  set_timeout(jwrapper = w, time.millisec = 1000L, sr.index = 0L)
 
-## ---- eval=n>0L----------------------------------------------------------
+## ------------------------------------------------------------------------
 #  raw.spectrum <- get_spectrum(jwrapper = w, sr.index = 0L, ch.index = 0L)
 #  head(raw.spectrum)
 #  summary(raw.spectrum)
 #  is_spectrum_valid(jwrapper = w, sr.index = 0L, ch.index = 0L)
 #  is_saturated(jwrapper = w, sr.index = 0L, ch.index = 0L)
 
-## ---- eval=n>0L----------------------------------------------------------
+## ------------------------------------------------------------------------
 #  close_all_spectrometers(w)
 
