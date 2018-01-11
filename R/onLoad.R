2cb755e3086d26fcfe877c4bae7b5f45c9346fd7
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2018-01-11 21:15:28 +0200

    Add start up messages, including information on OmniDriver library initialization.

--- R/onLoad.R
+++ R/onLoad.R
@@ -2,7 +2,7 @@
 #' 
 #' @param libname name of library (automatically supplied)
 #' @param pkgname name of the package (automatically supplied)
-#' @seealso see also \code{\link[rJava]{jpackage}}.
+#' @seealso see also \code{\link[rJava]{.jpackage}}.
 #' @keywords internal
 #' @return a numeric value
 #' 
@@ -11,18 +11,38 @@
 .onLoad <- function(libname, pkgname) {
   ooi_home <- Sys.getenv("OOI_HOME")
   if (is.null(ooi_home)) {
-    warning(
+    message <-
       paste(
-        "Path to OmniDriver runtime not found.",
-        "make sure that driver is installed and",
+        "rOmniDriver\n  Path to OmniDriver runtime not found.",
+        "\n  make sure that driver is installed and",
         "that the environment variable OOI_HOME is set.",
         sep = "\n"
       )
-    )
-    return()
+  } else {
+    ooi_path <- paste(ooi_home, "/OmniDriver.jar", sep = "")
+    
+    init.successful <-
+      rJava::.jpackage(pkgname,
+                       lib.loc = c(libname, ooi_home),
+                       morePaths = ooi_path)
+    
+    if (init.successful) {
+      message <- paste("rOmniDriver\n  OmniDriver initialization SUCCEEDED",
+                       "\n  OI_HOME: ", ooi_home,
+                       "\n  Path: ", ooi_path,
+                       sep = "")
+    } else {
+      message <- paste("rOmniDriver\n  OmniDriver initialization FAILED",
+                       "\n  OOI_HOME: ", ooi_home,
+                       "\n  Path: ", ooi_path,
+                       sep = "")
+    }
+    
   }
-  ooi_path <- paste(ooi_home, "/OmniDriver.jar", sep = "")
-  rJava::.jpackage(pkgname,
-                   lib.loc = c(libname, ooi_home),
-                   morePaths = ooi_path)
+  packageStartupMessage(message)
+}
+
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("  For news about '", pkgname, "', please, see http://www.r4photobiology.info/")
+  packageStartupMessage("  For on-line documentation see http://docs.r4photobiology.info/", pkgname, "/")
 }
